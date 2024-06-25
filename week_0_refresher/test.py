ea5d95df4207625452ebd139c08b5556ca989f02
Muzamil Bilwani <muzammil.bilwani@hotmail.com>
2024-06-25 21:42:11 +0500

    Longest common prefix

--- week_0_refresher/test.py
+++ week_0_refresher/test.py
@@ -1,18 +1,20 @@
+
+
 def longest_common_prefix(strs):
-    prefix = strs[0]
-    
+    prefix= strs[0]
+
     for string in strs[1:]:
         while string[:len(prefix)] != prefix and prefix:
             prefix = prefix[:len(prefix)-1]
         if not prefix:
             break
-            
     return prefix
+
+
+str = ["flow","flower","float"]
 
 # Input strings
 # str = input("Enter the strings: ").split()
-
-str = ["flower","flow","float"]
 
 result = longest_common_prefix(str)
 
